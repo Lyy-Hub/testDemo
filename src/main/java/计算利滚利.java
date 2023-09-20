c529f6b0476915b70a8cac7c9b9465a45aa73d4a
LiYueYang <13953193651@163.com>
2023-09-20 11:20:50 +0800

    利滚利方法优化

--- src/main/java/计算利滚利.java
+++ src/main/java/计算利滚利.java
@@ -5,17 +5,16 @@ import java.math.BigDecimal;
  */
 public class 计算利滚利 {
     public static void main(String[] args) {
-        double principal = 3800; // 本金
+        double principal = 100000; // 本金
         final double begin = principal;
-        double years = 30; // 年限
-        double interest = 0.0005; // 日利率
+        double years = 1; // 年限
+        double interest = 0.0001; // 日利率
         BigDecimal bigDecimal = null; // 格式转换
         // 把今天的本息之和作为明天的本金
         for (int i = 0; i < years * 366; i++) {
             principal += principal * interest;
             bigDecimal = new BigDecimal(principal + "");
         }
-        System.out.println(begin + "元利滚利（日利率：0.05%）," + years +
-                "年后本息总和为：" + bigDecimal + "元");
+        System.out.println(begin + "元利滚利（日利率：" + (interest * 100) + "%）," + years + "年后本息总和为：" + bigDecimal + "元");
     }
 }
